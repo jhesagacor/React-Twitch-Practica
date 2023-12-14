a18a07fb39111576df334bfdb3fcef609f502814
jherson <cordovajherson3@gmail.com>
2023-12-13 23:26:37 -0600

    second commit

--- src/components/StreamUsers.jsx
+++ src/components/StreamUsers.jsx
@@ -4,7 +4,7 @@ export default function StreamUsers(props) {
             <img src={props.image} alt=""/>
             <div>
             <p>{props.streamerName}</p>
-            <p>{props.streamerGame}</p>
+            <p className="streamer-gameName">{props.streamerGame}</p>
             </div>
             <div className="live-btn-container">{props.streamerStatus === 'live' && <a href="#" className="btn btn_live"><span className="live-icon-live"></span></a>}{props.streamerStatus === 'offline' && <a href="#" className="btn btn_live"><span className="live-icon-offline"></span></a>}</div>
         </article>
